1a4d04283529c03f8375c802daea3e3a87c38339
yan0422de <153173336+yan0422de@users.noreply.github.com>
2023-12-15 21:54:44 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,8 +1,7 @@
 //  Game.cpp
 //  PD_final_splendor
-//  Created by yan on 2023/12/13.
 
-#include "game.h"    
+#include "Game.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,8 +15,27 @@ using namespace std;
 Game::Game(){
     createCardDeck();
     setTable();
+    playGame();
 }
-//
+
+// play the game
+void Game::playGame(){
+    while(!game_over){
+        num = player1_turn ? 1 : 2;    // identify which player round
+       
+        get_command(num);
+        
+        gem_cnt_over_10(num);
+        
+        if(Player_Wins()){
+            clear_memory();
+            return;
+        }
+        player1_turn = !player1_turn;    // switch player
+    }
+}
+
+
 // create all cards
 void Game::createCardDeck()
 {
@@ -53,40 +71,64 @@ void Game::createCardDeck()
     return;
 }
 
-// set the 12 cards on the table
+/* set the 12 cards on the table. Set the first row of table with 4 cards in vendors, second with transport, and third with mines
+ */
 void Game::setTable(){
-    // set the first row of table with 4 cards in vendors, second with transport, and third with mines
     for(int i = 0; i < Card_COLS; i++){
         table[0][i] = vendors[19 - i];
         table[1][i] = transport[29 - i];
         table[2][i] = mines[39 - i];
     }
 }
-// play the game
-void Game::playGame(){
-    while(!game_over){
-        if(player1_turn){
-            get_command();
-        }
-    }
 
-
-
-
-
+void Game::get_command(int num){
+    // receive info. from frontend
+    players[num]->buyCard(table[0][0]);    // for test
+    return;
 }
 
+void Game::gem_cnt_over_10(int num){
+    int total_gem = players[num] -> getAllGemCnt();
+    
+    if(total_gem > 10){
+        if(total_gem == 11){
+            int color = 0;
+            //get_command(num);
+            players[num]->returnDiamond(color);
+        }
+        else if(total_gem == 12){
+            int color1 = 0, color2 = 0;
+            //get_command(num);
+            players[num]->returnDiamond(color1, color2);
+        }
+        else if(total_gem == 13){
+            int color1 = 0, color2 = 0, color3 = 0;
+            //get_command(num);
+            players[num]->returnDiamond(color1, color2, color3);
+        }
+    }
+    
+    return;
+}
 
-
-
-
-
-
-
-
-
-
-
+bool Game::Player_Wins(){
+    if(players[1] -> getPoint() >= 15){
+        // player 2 can play once
+        // get_command(num)
+        if(players[2] -> getPoint() < 15){
+            return true;
+        }
+        else{
+            isTied = true;    // tie
+            return true;
+        }
+    }
+    else if(players[2] -> getPoint() >= 15){
+        return true;
+    }
+    
+    return false;
+}
 int Game::ColorToIndex(string color) {
     //returns pertinent color
     if (color == "WHITE") {
@@ -109,7 +151,20 @@ int Game::ColorToIndex(string color) {
     }
     return 0;
 };
-int main(){
-    Game myGame;
-    return 0;
+
+void Game::clear_memory() {
+    for (auto card : mines) {
+        delete card;
+    }
+    mines.clear();
+    
+    for (auto card : transport) {
+        delete card;
+    }
+    transport.clear();
+
+    for (auto card : vendors) {
+        delete card;
+    }
+    vendors.clear();
 }
