bb7c980d63f3e472b9779bfe5ed57760dd93fb00
yan0422de <153173336+yan0422de@users.noreply.github.com>
2023-12-15 21:53:57 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,9 +1,10 @@
+// game.h
+
 #ifndef GAME_H
 #define GAME_H
 
 #include <iostream>
 #include <fstream>
-
 #include <string>
 #include "Cards.h"
 #include "Players.h"
@@ -13,27 +14,28 @@ using namespace std;
 
 class Game {
 private:
-    static const int Player_CNT = 2;
+    // init diamonds numuber
+    // int diamond_bank[6] = {4, 4, 4, 4, 4, 5};
+    
+    static const int Players_CNT = 2;
     static const int Card_ROWS = 3;
     static const int Card_COLS = 4;
     static const int GEMS = 6;
 
-
-    
-   
     // create the car deck
-
     void createCardDeck();
 
-    // set the table
+    // set the game table
     void setTable();
 
-
-    //constructor vars
+    // constructor vars
     int showedMinesCnt = 4;
     int showedTransportCnt = 4;
     int showedVendorsCnt = 4;
+    // represent player 1 or 2
+    int num = 0;
 
+    // store each level of cards
     vector<Card*> mines;
     vector<Card*> vendors;
     vector<Card*> transport;
@@ -41,14 +43,13 @@ private:
     //main game loop vars
     bool game_over = false;
     bool player1_turn = true;
+    bool isTied = false;
     
-    Player* players[Player_CNT];
+    Player* players[Players_CNT];
    
     Card* table[Card_ROWS][Card_COLS];
 
     //Main game loop functions
-    
-    string valid_move(int num);
 
     // 有個函數要更新被買走的牌（紀錄該等級的牌已經拿幾張出來），有個函數要檢查排堆（40, 30, 20）還有沒有牌
 
@@ -56,19 +57,20 @@ private:
 
     void update_grid(int num, int remove_gems[5], int gold);
 
-    void over_10();
+    void gem_cnt_over_10(int num);
 
-    void get_command();
+    void get_command(int num);
 
-    void Player_Wins();
+    bool Player_Wins();
 
 
     // Specific action functions
 
     int ColorToIndex(string color);
 
-    //clearing memory
+    // clearing memory
     void clear_memory();
+    
 public:
     //constructor
     Game();
@@ -80,3 +82,4 @@ public:
 };
 
 #endif
+
