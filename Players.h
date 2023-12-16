76f8df55e1d930dc13d4bd95f8d687c7a2b1ada3
hungpu1020 <153176998+hungpu1020@users.noreply.github.com>
2023-12-16 23:32:04 +0800

    Update Players.h

--- Players.h
+++ Players.h
@@ -15,21 +15,23 @@ private:
     Diamond myDiamonds;
     vector<Card*> myCards;
     vector<Card*> myReservedCards;
-    int bonus[MAX_BONUS_NUM];   // discount
+    int discount[MAX_BONUS_NUM];   // discount
     
 public:
     Player();
     // Getter
     int getAllGemCnt();
     int getPoint();
-    
+    // player actions
     void buyCard(Card* card);
     void reserveCard(Card* card);
     void buyReservedCard(Card* card);
     void spendByGOLD(const Card& card);
+
     void takeDiamond(int color);
     void takeDiamond(int color1, int color2, int color3);
-    void returnDiamond(int color); // only fieldDiamond can invoke
+
+    void returnDiamond(int color); 
     void returnDiamond(int color1, int color2);
     void returnDiamond(int color1, int color2, int color3);
 };
