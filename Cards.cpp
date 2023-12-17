1c0989c7577d9abae8128f97f036a9cc7e1c2798
yan0422de <153173336+yan0422de@users.noreply.github.com>
2023-12-17 12:15:21 +0800

    Update Cards.cpp

--- Cards.cpp
+++ Cards.cpp
@@ -6,9 +6,8 @@
 #include <iostream>
 
 
-Card::Card(int level, int color, int score, int white, int black, int red, int green, int blue)
- : level(level), color(color), score(score)
-{
+Card::Card(int level, int colorIdx, int score, int white, int black, int red, int green, int blue)
+ : level(level), color(colorIdx), score(score){
     // Use operator overloading, equals to spendDimd.diamonds[something]
     spendDimd[WHITE] = white;    
     spendDimd[BLACK] = black;
@@ -17,16 +16,14 @@ Card::Card(int level, int color, int score, int white, int black, int red, int g
     spendDimd[BLUE] = blue;
 }
 
-Card::Card(const Card& card)
-{
+Card::Card(const Card& card){
     this->level = card.level;
     this->color = card.color;
     this->score = card.score;
     this->spendDimd = card.spendDimd;
 }
 
-bool Card::operator==(const Card& card) const // Used to compare 2 cards when buyHoldCard
-{
+bool Card::operator==(const Card& card) const{ // Used to compare 2 cards when buyReservedCard
     if(this->level != card.level)
         return false;
     else if(this->color != card.color)
@@ -39,24 +36,18 @@ bool Card::operator==(const Card& card) const // Used to compare 2 cards when bu
         return true;
 }
 
-int Card::getScore()
-{
+int Card::getScore(){
     return this->score;
 }
 
-int Card::getColor()
-{
+int Card::getColor(){
     return this->color;
 }
 
-Diamond Card::getDiamond() const
-{
+Diamond Card::getDiamond() const{
     return this->spendDimd;
 }
 
-int Card::getEachDiamond(int idx) const
-{
+int Card::getEachDiamond(int idx) const{
     return this->spendDimd[idx];
 }
-
-
