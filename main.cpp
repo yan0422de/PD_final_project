fd13883a1809fdfc5925708fc8ddf8f966c52878
hungpu1020 <153176998+hungpu1020@users.noreply.github.com>
2023-12-17 15:33:23 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// main.cpp
+
+#include <iostream>
+#include <string>
+#include "game.h"
+using namespace std;
+
+
+int main(){
+    Game myGame;
+    // start the game
+    myGame.playGame();
+
+
+    return 0;
+}
